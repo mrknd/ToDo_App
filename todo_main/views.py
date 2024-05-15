5f34a2ef0742d701f5e172c3fbb231c43ceccd9a
mrkn <markiian.design@gmail.com>
2024-05-15 13:31:02 +0200

    todo app final version

--- todo_main/views.py
+++ todo_main/views.py
@@ -5,8 +5,10 @@ from todo_app.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('updated_at')
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('updated_at')
     context = {
-        'tasks': tasks
+        'tasks': tasks,
+        'completed_tasks': completed_tasks,
     }
     return render(request=request, template_name='home.html', context=context)
 
